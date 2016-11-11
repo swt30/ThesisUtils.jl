e554fab3b0d0edb0de9b764aca08d10f46630de7
Scott Thomas <swt30@ast.cam.ac.uk>
2016-11-11 15:21:17 +0000

    Add plot functionality and appearance

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # ThesisUtils
 
+This package controls the appearance of the plots in my thesis.
+
+## `autofig(plotfunc, name, size)`
+
+The function `autofig` wraps your plot command with the appropriate machinery to set the fonts, appearance and figure size appropriately.
+You provide it with a function that returns a plot, a name, and the desired size of the figure.
+The size can be any one of `Margin`, `Normal` or `Full`.
+`autofig` makes the plot, displays it, and also saves it as a pdf under the directory `autofigs/`.
+
+## `placeholder()`
+
+There is also a `placeholder` function that makes a placeholder plot, which you can use by calling `placeholder()`.
+
 [![Build Status](https://travis-ci.org/swt30/ThesisUtils.jl.svg?branch=master)](https://travis-ci.org/swt30/ThesisUtils.jl)
 
 [![Coverage Status](https://coveralls.io/repos/swt30/ThesisUtils.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/swt30/ThesisUtils.jl?branch=master)
--- src/ThesisUtils.jl
+++ src/ThesisUtils.jl
@@ -1,5 +1,117 @@
 module ThesisUtils
 
-# package code goes here
+import Plots: pyplot, plot, annotate!, font, with, png, svg
 
-end # module
+export Margin, Normal, Full
+export autofig, placeholder
+
+## Page layout settings
+# To describe the extent of a figure we define these terms
+@enum PlotSize Margin Normal Full
+const plotsizes = (Margin, Normal, Full)
+"Indicates that a figure spans the tufte-latex margin"
+Margin
+"Indicates that a figure spans the tufte-latex body area"
+Normal
+"Indicates that a figure spans the whole page"
+Full
+
+# DPI settings
+const dpi_default = 100
+const dpi_print = 300
+const dpi_scale = 170
+
+"Default plot aspect ratios"
+const aspectratio = Dict(
+  Margin => 1,
+  Normal => 1.5,
+  Full => 1.6 )
+
+"Widths of the margin, body text, and full page in inches"
+const width_in = Dict(
+  Margin => 2,
+  Normal => 4 + 3//16,
+  Full => 6 + 1//2 )
+
+"Sizes of figures in pixels"
+const figsize = let
+  pixelsizes = map(plotsizes) do s
+    base_aspect = [1, 1/aspectratio[s]]
+    dpi_scaled = base_aspect * dpi_print
+    width_scaled = dpi_scaled * width_in[s]
+    whole_numbers = round(Int, width_scaled)
+  end
+
+  Dict(zip(plotsizes, pixelsizes))
+end
+
+## Plot display options
+# Font settings
+const main_font = font("fbb", 10)
+const caption_font = font("fbb", 8)
+const main_font_opts = Dict(
+  :tickfont => main_font,
+  :legendfont => main_font,
+  :guidefont => main_font,
+  :titlefont => main_font )
+const caption_font_opts = Dict(
+  :tickfont => caption_font,
+  :legendfont => caption_font,
+  :guidefont => caption_font,
+  :titlefont => caption_font )
+
+# Plot appearance
+const plotopts = Dict(
+  :linewidth => 1.5,
+  :grid => false,
+  :dpi => dpi_scale)
+pyplot(;plotopts..., main_font_opts...)
+
+# Folder to save generated figures to
+const figdir = "autofigs"
+
+""" Make a plot to go in the thesis and save it as a pdf
+
+    plotfunc: The plotting function; should return a Plot object
+    name: The file name to save the figure as
+    s::PlotSize: One of `Margin`, `Normal` or `Full`. Indicates what size
+                 the figure should be saved as
+
+    vscale [optional, default=1]: Scale the plot to create more or less
+                                  vertical space
+    png [optional, default=1]: Save the plot as a png instead of a pdf """
+function autofig(plotfunc, name, s::PlotSize; vscale=1, savepng=false)
+  # get the figure size and font size
+  width = figsize[s][1]
+  height = figsize[s][2] * vscale
+  if s == Margin
+    font_opts = caption_font_opts
+  else
+    font_opts = main_font_opts
+  end
+
+  # plot the figure and save it to file
+  with(size=(width,height), font_opts...) do
+    p = plotfunc()
+    figpath = joinpath(figdir, name)
+    if savepng
+      png(figpath)
+    else
+      svg(figpath)
+      # bit of a workaround because the font doesn't embed
+      run(`rsvg-convert -f pdf -o $figpath.pdf $figpath.svg`)
+      rm("$figpath.svg")
+    end
+    p
+  end
+end
+
+"Make a default plot"
+function placeholder()
+  plot([sin, cos], linspace(0, 2π), labels=["sin(x)" "cos(x)"],
+    xlabel="This is the x-axis",
+    ylabel="This is the y-axis",
+    title="Placeholder plot")
+end
+
+end # module ThesisUtils
